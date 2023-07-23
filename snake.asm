d307413a95ecbe7b2e28944cac365e913d7f80ed
liquidsworrds <adithyasunil@pm.me>
2023-07-23 18:04:14 +0530

    snake movement

--- snake.asm
+++ snake.asm
@@ -13,13 +13,17 @@ SNAKECOLOR  equ 0x2020
 APPLECOLOR  equ 0x4020
 SNAKEARRAYX equ 0x1000
 SNAKEARRAYY equ 0x2000
+UP          equ 0
+DOWN        equ 1
+LEFT        equ 2
+RIGHT       equ 3
 
 ; Variables
 playerX:     dw 40
 playerY:     dw 12
 appleX:      dw 16
 appleY:      dw 8
-direction:   db 4
+direction:   db 3
 snakeLength: dw 1
 
 ; Game
@@ -59,12 +63,81 @@ game_loop:
         stosw
         add bx, 2
     loop .snake_loop
+
+    ; Draw Apple
+    imul di, [appleY], SCREENW*2
+    imul dx, [appleX], 2
+    add di, dx
+    mov ax, APPLECOLOR
+    stosw
+
+    ; Move the snake
+    mov al, [direction]
+    cmp al, UP
+    je move_up
+
+    mov al, [direction]
+    cmp al, DOWN
+    je move_down
+
+    mov al, [direction]
+    cmp al, LEFT
+    je move_left
+
+    mov al, [direction]
+    cmp al, RIGHT
+    je move_right
+
+    jmp update_snake
+
+    move_up:
+        dec word [playerY]  
+        jmp update_snake
+
+    move_down:
+        inc word [playerY]
+        jmp update_snake
+
+    move_left:
+        dec word [playerX]
+        jmp update_snake
         
+    move_right:
+        inc word [playerX]
 
-     
+    ; Update snake segments from back to front
+    update_snake:
+        imul bx, [snakeLength], 2 ; Each segment is 2 bytes
+            .snake_update_loop:
+                mov ax, [SNAKEARRAYX-2+bx]    
+                mov word [SNAKEARRAYX+bx], ax ; X of last segment in ax is moved to the one before it 
+               
+                mov ax, [SNAKEARRAYY-2+bx]
+                mov word [SNAKEARRAYY+bx], ax ; Y of last segment in ax is moved to the one before it 
+
+                dec bx
+                dec bx
+            jne .snake_update_loop           ; Stops at head
+
+    ; Storing updated values of head to SNAKEARRAY
+    mov ax, [playerX]
+    mov word [SNAKEARRAYX], ax
+
+    mov ax, [playerY]
+    mov word [SNAKEARRAYY], ax
+
+    ; Redraw delay
+    delay_loop:
+    mov bx, [TIMER]
+    inc bx
+    inc bx
+    .delay:
+        cmp [TIMER], bx
+        jl .delay
 
 
-    jmp game_loop
+jmp game_loop
+
 
 ; Bootsector
 times 510 - ($-$$) db 0
